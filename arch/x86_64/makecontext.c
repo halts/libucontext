148ec4c5b653a4f602d1f02ff5b02cfaf39f46f9
osy <osy86@users.noreply.github.com>
2020-01-31 20:20:39 -0800

    x86_64 support for library

--- arch/x86_64/makecontext.c
+++ arch/x86_64/makecontext.c
@@ -10,36 +10,48 @@
  * from the use of this software.
  */
 
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
 #include <stddef.h>
 #include <stdarg.h>
+#ifdef CUSTOM_IMPL
+#include <libucontext.h>
+#include "defs.h"
+#define UCONTEXT_T libucontext_ucontext_t
+#define GREG_T libucontext_greg_t
+#else
+#define UCONTEXT_T ucontext_t
+#define GREG_T greg_t
 #include <signal.h>
 #include <string.h>
 #include <stdint.h>
+#endif
+#include "defs.h"
 
 
-extern void __start_context(void);
+extern void _start_context(void);
 
 
 void
-__makecontext(ucontext_t *ucp, void (*func)(void), int argc, ...)
+libucontext_makecontext(UCONTEXT_T *ucp, void (*func)(void), int argc, ...)
 {
-	greg_t *sp;
+	GREG_T *sp;
 	va_list va;
 	int i;
 	unsigned int uc_link;
 
 	uc_link = (argc > 6 ? argc - 6 : 0) + 1;
 
-	sp = (greg_t *) ((uintptr_t) ucp->uc_stack.ss_sp + ucp->uc_stack.ss_size);
+	sp = (GREG_T *) ((uintptr_t) ucp->uc_stack.ss_sp + ucp->uc_stack.ss_size);
 	sp -= uc_link;
-	sp = (greg_t *) (((uintptr_t) sp & -16L) - 8);
+	sp = (GREG_T *) (((uintptr_t) sp & -16L) - 8);
 
 	ucp->uc_mcontext.gregs[REG_RIP] = (uintptr_t) func;
 	ucp->uc_mcontext.gregs[REG_RBX] = (uintptr_t) &sp[uc_link];
 	ucp->uc_mcontext.gregs[REG_RSP] = (uintptr_t) sp;
 
-	sp[0] = (uintptr_t) &__start_context;
+	sp[0] = (uintptr_t) &_start_context;
 	sp[uc_link] = (uintptr_t) ucp->uc_link;
 
 	va_start(va, argc);
@@ -48,25 +60,25 @@ __makecontext(ucontext_t *ucp, void (*func)(void), int argc, ...)
 		switch (i)
 		{
 		case 0:
-			ucp->uc_mcontext.gregs[REG_RDI] = va_arg (va, greg_t);
+			ucp->uc_mcontext.gregs[REG_RDI] = va_arg (va, GREG_T);
 			break;
 		case 1:
-			ucp->uc_mcontext.gregs[REG_RSI] = va_arg (va, greg_t);
+			ucp->uc_mcontext.gregs[REG_RSI] = va_arg (va, GREG_T);
 			break;
 		case 2:
-			ucp->uc_mcontext.gregs[REG_RDX] = va_arg (va, greg_t);
+			ucp->uc_mcontext.gregs[REG_RDX] = va_arg (va, GREG_T);
 			break;
 		case 3:
-			ucp->uc_mcontext.gregs[REG_RCX] = va_arg (va, greg_t);
+			ucp->uc_mcontext.gregs[REG_RCX] = va_arg (va, GREG_T);
 			break;
 		case 4:
-			ucp->uc_mcontext.gregs[REG_R8] = va_arg (va, greg_t);
+			ucp->uc_mcontext.gregs[REG_R8] = va_arg (va, GREG_T);
 			break;
 		case 5:
-			ucp->uc_mcontext.gregs[REG_R9] = va_arg (va, greg_t);
+			ucp->uc_mcontext.gregs[REG_R9] = va_arg (va, GREG_T);
 			break;
 		default:
-			sp[i - 5] = va_arg (va, greg_t);
+			sp[i - 5] = va_arg (va, GREG_T);
 			break;
 		}
 
@@ -74,4 +86,6 @@ __makecontext(ucontext_t *ucp, void (*func)(void), int argc, ...)
 }
 
 
-extern __typeof(__makecontext) makecontext __attribute__((weak, __alias__("__makecontext")));
+#ifndef CUSTOM_IMPL
+extern __typeof(libucontext_makecontext) makecontext __attribute__((weak, __alias__("libucontext_makecontext")));
+#endif
--- include/libucontext.h
+++ include/libucontext.h
@@ -1,6 +1,8 @@
 #ifndef __LIBUCONTEXT_H
 #define __LIBUCONTEXT_H
 
+#include <stdint.h>
+
 /* Structure describing a signal stack.  */
 typedef struct
   {
@@ -11,7 +13,7 @@ typedef struct
 
 typedef unsigned long int libucontext_sigset_t;
 
-#ifdef __aarch64__
+#if defined(__aarch64__)
 /* Context to describe whole processor state.  This only describes
    the core registers; coprocessor registers get saved elsewhere
    (e.g. in uc_regspace, or somewhere unspecified on the stack
@@ -29,9 +31,6 @@ typedef struct
        arch/arm64/include/uapi/asm/sigcontext.h linux header for details.  */
     unsigned char __reserved[4096] __attribute__ ((__aligned__ (16)));
   } libucontext_mcontext_t;
-#else
-#error "mcontext not implemented!"
-#endif
 
 /* Userlevel context.  */
 typedef struct libucontext_ucontext
@@ -42,6 +41,67 @@ typedef struct libucontext_ucontext
     libucontext_sigset_t uc_sigmask;
     libucontext_mcontext_t uc_mcontext;
   } libucontext_ucontext_t;
+#elif defined(__x86_64__)
+/* Type for general register.  */
+typedef long long int libucontext_greg_t;
+
+/* Container for all general registers.  */
+typedef libucontext_greg_t libucontext_gregset_t[23];
+
+struct libucontext_fpxreg
+{
+  unsigned short int significand[4];
+  unsigned short int exponent;
+  unsigned short int __glibc_reserved1[3];
+};
+
+struct libucontext_xmmreg
+{
+  uint32_t  element[4];
+};
+
+struct libucontext_fpstate
+{
+  /* 64-bit FXSAVE format.  */
+  uint16_t    cwd;
+  uint16_t    swd;
+  uint16_t    ftw;
+  uint16_t    fop;
+  uint64_t    rip;
+  uint64_t    rdp;
+  uint32_t    mxcsr;
+  uint32_t    mxcr_mask;
+  struct libucontext_fpxreg _st[8];
+  struct libucontext_xmmreg _xmm[16];
+  uint32_t    __libucontext_reserved1[24];
+};
+
+/* Structure to describe FPU registers.  */
+typedef struct libucontext_fpstate *libucontext_fpregset_t;
+
+/* Context to describe whole processor state.  */
+typedef struct libucontext_mcontext
+  {
+    libucontext_gregset_t gregs;
+    /* Note that fpregs is a pointer.  */
+    libucontext_fpregset_t fpregs;
+    unsigned long long __reserved1 [8];
+} libucontext_mcontext_t;
+
+/* Userlevel context.  */
+typedef struct libucontext_ucontext
+  {
+    unsigned long int uc_flags;
+    struct libucontext_ucontext *uc_link;
+    libucontext_stack_t uc_stack;
+    libucontext_mcontext_t uc_mcontext;
+    libucontext_sigset_t uc_sigmask;
+    struct libucontext_fpstate __fpregs_mem;
+    unsigned long long int __ssp[4];
+  } libucontext_ucontext_t;
+#else
+#error "mcontext not implemented!"
+#endif
 
 /* Get user context and store it in variable pointed to by UCP.  */
 int libucontext_getcontext (libucontext_ucontext_t *__ucp);
